43c54ae006fcc10ed181e4f13c856ecc4467c19c
SINGH16Utkarsh <csaimlews2210@glbitm.ac.in>
2025-08-29 14:43:32 +0530

    Time: 19 ms (96.31%) | Memory: 49.8 MB (90.45%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,20 +1,20 @@
 class KthLargest {
-     private PriorityQueue<Integer> heap;
-        private int k;
+    private PriorityQueue<Integer> minheap;
+    private int k;
     public KthLargest(int k, int[] nums) {
-        this.k=k;
-        heap = new  PriorityQueue<>();
-        for(int num : nums){
-            add(num);
-        } 
+       this.k = k;
+       minheap = new PriorityQueue<>();
+       for(int num : nums){
+        add(num);
+       }
     }
     
     public int add(int val) {
-     heap.offer(val);
-     if(heap.size () >k) {
-        heap.poll();
-     }
-     return heap.peek();  
+        minheap.offer(val);
+        if(minheap.size()>k){
+            minheap.poll();
+        }
+        return minheap.peek();
     }
 }
 
